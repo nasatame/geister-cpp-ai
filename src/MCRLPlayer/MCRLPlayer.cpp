e68bcbc003e5008d901876abefcd9fdb451ca718
nasatame <s1810741@edu.cc.uec.ac.jp>
2021-01-12 16:36:32 +0900

    MCRLPlayer first commit

---
+++ src/MCRLPlayer/MCRLPlayer.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include "MonteCarloPlayer.hpp"
+
+MonteCarloPlayer player;
+
+extern "C"{
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+std::string decideHand(const std::string& res){
+    return player.decideHand(res);
+}
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+std::string decideRed(){
+    player.initialize();
+    return player.decideRed();
+}
+}
